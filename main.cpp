0a9e33a37da1102e673067b496ba12fe3db67764
Yang Liu <summerliu2333@gmail.com>
2026-05-19 16:42:08 -0700

    M1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <list>
+#include <string>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
+struct Customer {
+    string name;
+    string order;
+};
+
 int main() {
+    srand(time(0));
+ 
+    string names[] = {"Jake", "Mia", "Tyler", "Sara", "Nate", "Chloe", "Marcus", "Lily", "Derek", "Amy"};
+    string coffees[] = {"Iced Latte", "Black Coffee", "Flat White", "Vanilla Latte", "Cold Brew"};
+ 
+    //M1
+    list<Customer> coffeeQueue;
+    for (int i = 0; i < 3; i++) {
+        Customer c;
+        c.name = names[rand() % 10];
+        c.order = coffees[rand() % 5];
+        coffeeQueue.push_back(c);
+    }
+ 
+    for (auto entry : coffeeQueue) {
+        cout << entry.name << " - " << entry.order << endl;
+    }
+
     return 0;
 }
